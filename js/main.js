d96ffea34c3500a8204feb77a56bc9138b351c40
Claudio Gomboli <c.gomboli@gmail.com>
2017-04-05 17:34:38 +0100

    Remove jQuery
    Remove jQuery

--- js/main.js
+++ js/main.js
@@ -1,35 +1,55 @@
-$(document).ready(function () {	
-	
-	$(document).on('click', '.showgrid', function(){
-	  $(".hidegrid").show();
-	  $("#grid").fadeIn();
-	  $(this).hide();
-	});
-	
-	$(document).on('click', '.hidegrid', function(){
-	  $(".showgrid").show();
-	  $("#grid").fadeOut();
-	  $(this).hide();
-	});
-	
-	$(document).on('click', '.shape', function(){
-		var shapetype = $(this).attr('class');
-	  $('.shape').removeClass('current');
-	  $('#icona').removeClass('current');
-	  $('#icona').removeClass('ios');
-	  $('#icona').removeClass('square');
-	  $('#icona').removeClass('circle');
-	  $('#icona').removeClass('squircle');
-	  $('#icona').removeClass('roundsquare');
-	  $(this).addClass('current');
-	  $('#icona').addClass(shapetype);
+document.onreadystatechange = function () {
+  if (document.readyState === "interactive") {
 	  
-	  if ($('#icona').hasClass("square")) {
-	  	$('.iconwrap').css('background','#FFC200');
-	  }
-	  else {
-		  $('.iconwrap').css('background','none');
-	  }
-	});
+		const $ = str => document.querySelector(str);
+			
+		$(".showgrid").onclick = function() {
+		  $(".hidegrid").style.opacity = "1";
+		  document.getElementById("grid").style.opacity = "1";
+		  $(".showgrid").style.opacity = "0";
+		}
 		
-});
+		$(".hidegrid").onclick = function() {
+		  $(".showgrid").style.opacity = "1";
+		  $("#grid").style.opacity = "0";
+		  $(".hidegrid").style.opacity = "0";
+		}
+		
+		$("#square").onclick = function() {
+		  $(".iconwrap").style.background = "#FFC200";
+		  $("#square").classList.add('.square');
+		  $('.shape').classList.remove('.current');
+		  $("#square").classList.add('.current');
+		}
+		
+		$("#ios").onclick = function() {
+		  $(".iconwrap").style.background = "none";
+		  $("#ios").classList.add('.ios');
+		  $('.shape').classList.remove('.current');
+		  $("#ios").classList.add('.current');
+		}
+		
+		$("#circle").onclick = function() {
+		  $(".iconwrap").style.background = "none";
+		  $("#circle").classList.add('.circle');
+		  $('.shape').classList.remove('.current');
+		  $("#circle").classList.add('.current');
+		}
+		
+		$("#squircle").onclick = function() {
+		  $(".iconwrap").style.background = "none";
+		  $("#squircle").classList.add('.squircle');
+		  $('.shape').classList.remove('.current');
+		  $("#squircle").classList.add('.current');
+		  alert('dfd');
+		}
+		
+		$("#roundsquare").onclick = function() {
+		  $(".iconwrap").style.background = "none";
+		  $("#roundsquare").classList.add('.roundsquare');
+		  $('.shape').classList.remove('.current');
+		  $("#roundsquare").classList.add('.current');
+		}
+	 	
+  }
+}
